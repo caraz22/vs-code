687be7968206fead9e2ccc6bb1bdc58f20974440
caraz22 <cszoke22@gmail.com>
2025-06-30 17:00:55 -0500

    configured CMakeLists.txt

---
+++ escape-the-valley/build/CMakeFiles/escape-the-valley.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/escape-the-valley.dir/src/main.cpp.obj"
+  "CMakeFiles/escape-the-valley.dir/src/main.cpp.obj.d"
+  "escape-the-valley.exe"
+  "escape-the-valley.exe.manifest"
+  "escape-the-valley.pdb"
+  "libescape-the-valley.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/escape-the-valley.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ escape-the-valley/build/CMakeFiles/escape-the-valley.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for escape-the-valley.
+# This may be replaced when dependencies are built.
